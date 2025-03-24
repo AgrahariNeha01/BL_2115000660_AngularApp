e910f2e69d0ff366c32c0d41448ce67338f4ed7e
Neha Agrahari <neha16agrahari@gmail.com>
2025-03-24 15:20:43 +0530

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
-  imgUrl ="../assets/BL_logo_square_jpg.jpg";
+  title = 'helloworld';
+  imgUrl = "./assets/BL_logo_square_jpg.jpg";
 
-  ngOnInit(): void {
-    this.title = "Hello from BridgeLabz.";      // it is called after component is initialized, use when dynamic value    
+  ngOnInit(): void 
+  {
+    this.title ="hello form bridgelabz";
   }
-  // message: string = "Hello from BridgeLabz! 🚀"; // Template Expression Variable,  message is initialized when the component is created,    use when static values
 }
