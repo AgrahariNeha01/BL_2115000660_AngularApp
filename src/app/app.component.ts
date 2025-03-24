af354db8a3b899a29f7ea0a676acd6baa6dacf7b
Neha Agrahari <neha16agrahari@gmail.com>
2025-03-24 14:00:50 +0530

    uc2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,20 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+  title="Helloworld";
+  imgUrl="assets/BL_logo_sqaure_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  
+  ngOnInit(): void{
+    this.title= "hello from bridgelabz"
+  }
+  onClick(Sevent: Event) { 
+    console.log("Save button is clicked!", Sevent);
+    window.open(this.url, "_blank"); 
+  }
 }
